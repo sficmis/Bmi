5ec7f7915c42f02536bdbaa68b076febd6dfdb3b
sunjames-me <sunjames@me.com>
2024-04-26 17:47:22 +0800

    BMI

--- app/src/main/java/com/sunjames/bmi/Hello.kt
+++ app/src/main/java/com/sunjames/bmi/Hello.kt
@@ -6,6 +6,14 @@ class Hello {
 fun main() {
     val p = Person()
     p.hello()
+    p.weight = 65.5f
+    p.height = 1.78f
+    println(p.bmi())
+    //BMI
+    val weight = 74.5f
+    val height = 1.78f
+    val bmi = weight / (height * height)
+    println("BMI is: " + bmi)
 
     println("Hello Kotlin!")
 //    val age = 20
--- app/src/main/java/com/sunjames/bmi/Person.kt
+++ app/src/main/java/com/sunjames/bmi/Person.kt
@@ -1,6 +1,12 @@
 package com.sunjames.bmi
 
 class Person {
+    var weight : Float = 0f
+    var height : Float = 0f
+    fun bmi() :Float {
+        val bmi = weight / (height * height)
+        return bmi
+    }
     fun hello(){
         println("Hello!")
     }
